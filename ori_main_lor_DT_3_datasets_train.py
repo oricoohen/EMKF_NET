b93c1d6c243c59587c0024a0b15fba6e64dbecf5
orico <orico@post.bgu.ac.il>
2026-05-20 20:14:36 +0300

    start lorenz experiement

--- ori_main_lor_DT_3_datasets_train.py
+++ ori_main_lor_DT_3_datasets_train.py
@@ -82,7 +82,7 @@ torch.manual_seed(1)
 cycles = 10  # Number of datasets (each represents 30 timesteps with different F)
 num_em_iters = 2
 # noise q and r
-r2 = torch.tensor([0.01], device=device)  # [100, 10, 1, 0.1, 0.01]
+r2 = torch.tensor([0.001], device=device)  # [100, 10, 1, 0.1, 0.01]
 vdB = -20  # ratio v=q2/r2
 v = 10 ** (vdB / 10)
 q2 = torch.mul(v, r2)
@@ -98,20 +98,20 @@ sys_model.InitSequence(m1x_0, m2x_0)  # x0 and P0
 print("\n" + "="*80)
 print("GENERATING 3 DATASETS WITH DIFFERENT H MATRICES (F IS FIXED)")
 print("="*80)
-load_path_rtsnet_full = 'RTSNet/lorenz_rotated_001/3datasets/RTSNet_full.pt'
-load_path_rtsnet_partial = 'RTSNet/lorenz_rotated_001/3datasets/RTSNet_partial.pt'
-load_path_rtsnet_partial_joint ='RTSNet/lorenz_rotated_001/3datasets/RTSNet_partial_joint.pt'
-load_path_M_joint = 'RTSNet/lorenz_rotated_001/3datasets/M_step_net_joint.pt'
-destination_path_rtsnet_full = 'RTSNet/lorenz_rotated_001/10datasets/or_RTSNet_full.pt'
-destination_path_rtsnet_partial = 'RTSNet/lorenz_rotated_001/10datasets/ori_RTSNet_partial.pt'
-destination_path_M_reg = 'RTSNet/lorenz_rotated_001/10datasets/ori_M_step_net.pt'
-destination_path_M_joint = 'RTSNet/lorenz_rotated_001/10datasets/ori_M_step_net_joint.pt'
-destination_path_rtsnet_partial_joint = 'RTSNet/lorenz_rotated_001/ori_datasets/10RTSNet_partial_joint.pt'
+load_path_rtsnet_full = 'RTSNet/lorenz_rotated_0001/3datasets/RTSNet_full.pt'
+load_path_rtsnet_partial = 'RTSNet/lorenz_rotated_0001/3datasets/RTSNet_partial.pt'
+load_path_rtsnet_partial_joint ='RTSNet/lorenz_rotated_0001/3datasets/RTSNet_partial_joint.pt'
+load_path_M_joint = 'RTSNet/lorenz_rotated_0001/3datasets/M_step_net_joint.pt'
+destination_path_rtsnet_full = 'RTSNet/lorenz_rotated_0001/10datasets/ori_RTSNet_full.pt'
+destination_path_rtsnet_partial = 'RTSNet/lorenz_rotated_0001/10datasets/ori_RTSNet_partial.pt'
+destination_path_M_reg = 'RTSNet/lorenz_rotated_0001/10datasets/ori_M_step_net.pt'
+destination_path_M_joint = 'RTSNet/lorenz_rotated_0001/10datasets/ori_M_step_net_joint.pt'
+destination_path_rtsnet_partial_joint = 'RTSNet/lorenz_rotated_0001/ori_datasets/10RTSNet_partial_joint.pt'
 # destination_path_M_joint10 = 'RTSNet/lorenz_rotated_10/3datasets/M_step_net_jointb.pt'
 # destination_path_rtsnet_partial_joint10 = 'RTSNet/lorenz_rotated_10/3datasets/RTSNet_partial_jointb.pt'
 # destination_path_M_joint = 'RTSNet/lorenz_rotated_10/3datasets/M_step_net_joint2h.pt'
 # destination_path_rtsnet_partial_joint = 'RTSNet/lorenz_rotated_10/3datasets/RTSNet_partial_jointb.pt'
-bigru_path = 'RTSNet/lorenz_rotated_001/10datasets/benchmarks/bigru_smoother1_datasets.pt'
+bigru_path = 'RTSNet/lorenz_rotated_0001/10datasets/benchmarks/bigru_smoother1_datasets.pt'
 for p in [
     destination_path_rtsnet_full,
     destination_path_rtsnet_partial,
